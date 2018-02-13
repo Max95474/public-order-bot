8d3488490f26b2d0fd0b58b4f871ab5b36f21074
Alex <alexey.shulev@gmail.com>
2018-02-14 01:12:45 +0200

    connected to telegram api

--- main.go
+++ main.go
@@ -1,9 +1,35 @@
 package main
 
 import (
-	"fmt"
+	"github.com/Syfaro/telegram-bot-api"
+	"log"
 )
 
 func main() {
-	fmt.Println("Hello world!")
+	bot, err := tgbotapi.NewBotAPI("520708190:AAHGJshPWGeDleVkmEPaCvUphErEvc5fTps")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates, err := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message == nil {
+			continue
+		}
+
+		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Poshel nahui")
+		msg.ReplyToMessageID = update.Message.MessageID
+
+		bot.Send(msg)
+	}
 }
