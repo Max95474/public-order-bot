2cedbcd5a23b9b268ff05af39d1a3239e544fb46
Alex <alexey.shulev@gmail.com>
2018-02-15 19:20:09 +0200

    mocked commands

--- telegram/commands.go
+++ telegram/commands.go
@@ -4,26 +4,54 @@ import (
 )
 
 func getResponse(command string) string {
-	
+  
   switch command {
-		case "create": {
-			return createOrder()
-		}
-	  case "list": {
+    case "create": {
+      return createOrder()
+    }
+    case "list": {
       return ordersList()
-	  }
-		default: {
-			return "Poshel nahui"
-		}
-	}
+    }
+    case "add": {
+      return addItem()
+    }
+    case "remove": {
+      return removeItem()
+    }
+    case "result": {
+      return orderResult()
+    }
+    case "close": {
+      return closeOrder()
+    }
+    default: {
+      return "Poshel na hui"
+    }
+  }
 }
 
 
 
 func createOrder() string {
-	return "Order Created"
+  return "Order Created"
 }
 
 func ordersList() string {
   return "Orders list"
+}
+
+func addItem() string {
+  return "Item added to order"
+}
+
+func removeItem() string {
+  return "Item removed from order"
+}
+
+func orderResult() string {
+  return "All added items"
+}
+
+func closeOrder() string {
+  return "Order closed"
 }
--- telegram/telegram.go
+++ telegram/telegram.go
@@ -1,41 +1,41 @@
 package telegram
 
 import (
-	"github.com/Syfaro/telegram-bot-api"
-	"log"
+  "github.com/Syfaro/telegram-bot-api"
+  "log"
 )
 
 
 func Start(apiKey string, debug bool) {
-	bot, err := tgbotapi.NewBotAPI(apiKey)
-	
+  bot, err := tgbotapi.NewBotAPI(apiKey)
+  
   if err != nil {
-		log.Panic(err)
-	}
+    log.Panic(err)
+  }
 
-	bot.Debug = debug;
+  bot.Debug = debug;
 
-	log.Printf("Authorized on account %s", bot.Self.UserName)
+  log.Printf("Authorized on account %s", bot.Self.UserName)
 
-	u := tgbotapi.NewUpdate(0)
-	u.Timeout = 60
+  u := tgbotapi.NewUpdate(0)
+  u.Timeout = 60
 
-	updates, err := bot.GetUpdatesChan(u)
+  updates, err := bot.GetUpdatesChan(u)
 
-	for update := range updates {
-		if update.Message == nil {
-			continue
-		}
+  for update := range updates {
+    if update.Message == nil {
+      continue
+    }
 
-		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
-		
+    log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+    
     command := ejectCommand(update.Message.Text, bot.Self.UserName)
 
-		response := getResponse(command)
+    response := getResponse(command)
 
-		msg := tgbotapi.NewMessage(update.Message.Chat.ID, response)
-		msg.ReplyToMessageID = update.Message.MessageID
+    msg := tgbotapi.NewMessage(update.Message.Chat.ID, response)
+    msg.ReplyToMessageID = update.Message.MessageID
 
-		bot.Send(msg)
-	}
+    bot.Send(msg)
+  }
 }
